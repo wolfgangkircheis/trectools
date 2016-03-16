547080e9c43ecde97e263f94cfd021335b38dcfd
joaopalotti <joaopalotti@gmail.com>
2016-03-16 17:45:09 +0100

    Added simple way to run trec_eval.

--- trectools/trec_tools.py
+++ trectools/trec_tools.py
@@ -2,7 +2,9 @@
 # encoding: utf-8
 
 # Standard libraries
-from subprocess import call
+# from subprocess import call # TODO: change os.system to subprocess
+# TODO: use logging properly
+import logging
 import os
 
 # External libraries
@@ -173,6 +175,9 @@ class trec_run:
         # TODO: use subprocess.
         if not outfile:
             outfile = self.get_filename() + ".res"
-        os.system("trec_eval -q %s %s > %s" % (a_trec_qrel.get_filename(), self.get_filename(), outfile))
+        cmd = "trec_eval -q %s %s > %s" % (a_trec_qrel.get_filename(), self.get_filename(), outfile)
+
+        logging.warning("Running: %s " % (cmd))
+        os.system(cmd)
         return trec_res(outfile)
 
