575c7358f410e61ea3fad425989e68327143abc1
Joao Palotti <joaopalotti@gmail.com>
2019-01-31 16:30:13 +0300

    Added rbp fusion

--- trectools/fusion.py
+++ trectools/fusion.py
@@ -121,9 +121,31 @@ def reciprocal_rank_fusion(trec_runs, k=60, max_docs=1000, output=sys.stdout):
             output.write("%s Q0 %s %d %f reciprocal_rank_fusion_k=%d\n" % (str(topic), docid, rank, score, k))
 
 
+def rank_biased_precision_fusion(trec_runs, p=0.80, max_docs=1000, output=sys.stdout):
+    """
+        Implements a rank biased precision (RBP) fusion
+
+        Parameters:
+            p: persistence parameter of RBP (default = 0.80)
+            output: a file pointer to write the results. Sys.stdout is the default.
+    """
+    topics = trec_runs[0].topics()
+
+    for topic in sorted(topics):
+        doc_scores = {}
+        for r in trec_runs:
+            docs_for_run = r.get_top_documents(topic, n=1000)
+
+            for pos, docid in enumerate(docs_for_run, start=1):
+                doc_scores[docid] = doc_scores.get(docid, 0.0) + (1.0-p) * (p) ** (pos-1)
+
+        # Writes out information for this topic
+        for rank, (docid, score) in enumerate(sorted(doc_scores.iteritems(), key=lambda x:(-x[1],x[0]))[:max_docs], start=1):
+            output.write("%s Q0 %s %d %f rank_biased_precision_fusion_p=%.3f\n" % (str(topic), docid, rank, score, p))
+
+
 def borda_count(trec_runs):
     print "TODO: BordaCount (Aslam & Montague, 2001)"
-
 
 def svp(trec_runs):
     print "TODO: (Gleich & Lim, 2011)"
